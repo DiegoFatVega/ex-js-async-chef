6d3d4f89ac8c34fc872d88ba7ba99c0226c2e7b6
DiegoFatVega <fatvegadiego@gmail.com>
2026-02-07 10:25:49 +0100

    implement async functions to fetch recipe and chef birthday data

---
+++ ex-js-async-chef.js
@@ -0,0 +1,18 @@
+async function fetchJson(url) { //funzione di supporto per evitare di dover fare 2 await e ci ritorna l`oggetto trasformato in Json
+    const res = await fetch(url);
+    const obj = await res.json();
+    return obj
+}
+
+async function getChefbirthday(id) {
+    const recipe = await fetchJson(`https://dummyjson.com/recipes/${id}`);
+    const userId = recipe.userId;
+
+    const infoChef = await fetchJson(`https://dummyjson.com/users/${userId}`)
+    return infoChef.birthDate
+}
+
+(async () => {
+    const birthday = await getChefbirthday(1);
+    console.log("Data di nascita dello chef:", birthday)
+})();
